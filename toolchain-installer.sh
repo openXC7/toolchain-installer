19e34390b7419a79ef7f48004aa5898e5293e610
Hans Baier <hansfbaier@gmail.com>
2023-03-30 04:56:40 +0700

    update to openxc7

--- toolchain-installer.sh
+++ toolchain-installer.sh
@@ -4,7 +4,7 @@ TMPDIR=$(mktemp -d)
 cd $TMPDIR
 
 YOSYS_VERSION=0.17
-NEXTPNR_VERSION=0.3.0
+NEXTPNR_VERSION=0.4.0
 
 function cleanup {
   cd
@@ -13,7 +13,7 @@ function cleanup {
 
 if [ ! -x "$(which yosys)" ]
 then
-  wget -c https://github.com/kintex-chatter/yosys-snap/releases/download/v${YOSYS_VERSION}/yosys_${YOSYS_VERSION}_amd64.snap
+  wget -c https://github.com/openXC7/yosys-snap/releases/download/v${YOSYS_VERSION}/yosys_${YOSYS_VERSION}_amd64.snap
   if [ $? -ne 0 ]
   then
     echo Downloading yosys package failed.
@@ -27,58 +27,66 @@ fi
 
 if [ -d /snap/nextpnr-kintex ]
 then
-  echo nextpnr-kintex is already installed. Please remove it first, before running this script!
+  echo removing old toolchain version
+  snap remove nextpnr-kintex
   cleanup
   exit 1
 fi
 
-wget -c https://github.com/kintex-chatter/nextpnr-kintex-snap/releases/download/v${NEXTPNR_VERSION}/nextpnr-kintex_${NEXTPNR_VERSION}_amd64.snap
-if [ $? -ne 0 ]
+if [ -d /snap/openxc7 ]
 then
-  echo Downloading nextpnr-kintex package failed.
+  echo openxc7 is already installed. Please remove it first, before running this script!
   cleanup
   exit 1
 fi
 
-sudo snap install --classic --dangerous ./nextpnr-kintex_*.snap
+wget -c https://github.com/openXC7/openXC7-snap/releases/download/v${NEXTPNR_VERSION}/openxc7_${NEXTPNR_VERSION}_amd64.snap
 if [ $? -ne 0 ]
 then
-  echo installing nextpnr-kintex package failed.
+  echo Downloading openxc7 package failed.
+  cleanup
+  exit 1
+fi
+
+sudo snap install --classic --dangerous ./openxc7_*.snap
+if [ $? -ne 0 ]
+then
+  echo installing openxc7 package failed.
   cleanup
   exit 1
 fi
 
 if [ ! -x "$(which nextpnr-xilinx)" ]
 then
-  sudo snap alias nextpnr-kintex.nextpnr-xilinx nextpnr-xilinx
+  sudo snap alias openxc7.nextpnr-xilinx nextpnr-xilinx
 else
   echo nextpnr-xilinx already exists, refraining from creating an alias
 fi
 
 if [ ! -x "$(which bbasm)" ]
 then
-  sudo snap alias nextpnr-kintex.bbasm bbasm
+  sudo snap alias openxc7.bbasm bbasm
 else
   echo bbasm already exists, refraining from creating an alias
 fi
 
 if [ ! -x "$(which fasm2frames)" ]
 then
-  sudo snap alias nextpnr-kintex.fasm2frames fasm2frames
+  sudo snap alias openxc7.fasm2frames fasm2frames
 else
   echo fasm2frames already exists, refraining from creating an alias
 fi
 
 if [ ! -x "$(which xc7frames2bit)" ]
 then
-  sudo snap alias nextpnr-kintex.xc7frames2bit xc7frames2bit
+  sudo snap alias openxc7.xc7frames2bit xc7frames2bit
 else
   echo xc7frames2bit already exists, refraining from creating an alias
 fi
 
 if [ ! -x "$(which bit2fasm)" ]
 then
-  sudo snap alias nextpnr-kintex.bit2fasm bit2fasm
+  sudo snap alias openxc7.bit2fasm bit2fasm
 else
   echo bit2fasm already exists, refraining from creating an alias
 fi
